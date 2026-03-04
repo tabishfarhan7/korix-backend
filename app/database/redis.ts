29dd822f7fbf5dec949a686032365607e3c5c5be
fahad <fahadkhanf715@gmail.com>
2026-03-04 16:18:54 +0530

    set the redis databse index

--- app/database/redis.ts
+++ app/database/redis.ts
@@ -3,15 +3,16 @@ import { REDIS_URL } from '../config/env.js';
 
 const redisClient = createClient({
     url: REDIS_URL,
+    database: 1,
 });
 
 redisClient.on('error', (err) => {
-    console.error('❌ Redis connection error:', err);
+    console.error('Redis connection error:', err);
 });
 
 export const connectRedis = async (): Promise<void> => {
     await redisClient.connect();
-    console.log('✅ Redis connected successfully');
+    console.log('Redis connected successfully');
 };
 
 export default redisClient;
